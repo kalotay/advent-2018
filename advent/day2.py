7c4f50c8b3e0561d9c2291458e4e276ea27fbed0
Dan Kalotay <dan.kalotay@masabi.com>
2018-12-02 17:25:51 +0100

    solve part 2 of day 2

--- advent/day2.py
+++ advent/day2.py
@@ -17,6 +17,27 @@ def _getcounts(id):
     counter = Counter(id)
     return frozenset(counter.values())
 
+def commonletters(ids):
+    ids_list = list(ids)
+    for index, id in enumerate(ids_list):
+        for other in ids_list[index:]:
+            common = []
+            mismatches = 0
+            for left, right in zip(id, other):
+                if left == right:
+                    common.append(left)
+                else:
+                    mismatches += 1
+                if mismatches > 1:
+                    break
+            if mismatches == 1:
+                return ''.join(common)
+    return None
+
 def solve1(filename):
     with open(filename) as f:
         return checksum(f)
+
+def solve2(filename):
+    with open(filename) as f:
+        return commonletters(f)
--- tests/test_day2.py
+++ tests/test_day2.py
@@ -1,5 +1,5 @@
 import unittest
-from advent.day2 import checksum
+from advent.day2 import checksum, commonletters
 
 class TestDay2(unittest.TestCase):
 
@@ -7,4 +7,10 @@ class TestDay2(unittest.TestCase):
         test_data = ["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]
         expected = 12
         actual = checksum(test_data)
+        self.assertEqual(expected, actual)
+
+    def test_commonletters(self):
+        test_data = ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
+        expected = "fgij"
+        actual = commonletters(test_data)
         self.assertEqual(expected, actual)
